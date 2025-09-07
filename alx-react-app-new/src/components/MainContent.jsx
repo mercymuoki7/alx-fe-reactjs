dbd47c41b8c780970eed9cefd1e536f7476db0d6
Mercy Muoki <mercymuoki68@gmail.com>
2025-09-07 20:59:34 +0300

    Task 1: Apply inline CSS styling to components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,21 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#eef2f7",
+        minHeight: "200px",
+      }}
+    >
+      <h2 style={{ textAlign: "center", color: "#333" }}>
+        Welcome to My Company
+      </h2>
+      <p style={{ lineHeight: "1.6", color: "#555" }}>
+        Here is some information about what we do...
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
+
