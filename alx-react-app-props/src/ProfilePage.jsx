fef2c1f54cf8453209ba11507f50348f87a8c518
Mercy Muoki <mercymuoki68@gmail.com>
2025-09-07 21:29:51 +0300

    Consume context in ProfilePage instead of prop drilling

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,7 +1,16 @@
-import UserInfo from "./UserInfo";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
 function ProfilePage() {
-  return <UserInfo />;
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default ProfilePage;
